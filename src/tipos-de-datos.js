9c497006eb3ce3afe22c0905330d63b42179aec9
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-12-17 04:02:23 +0000

    Clase 5 del Curso de Fundamentos de JavaScript

---
+++ src/tipos-de-datos.js
@@ -0,0 +1,41 @@
+//PRIMITIVOS
+
+//Strings
+let nombre = "Tere";
+
+//number
+let edad = 56;
+
+//boolean
+let mayoriaDeEdad = false;
+
+//null
+let nulo = null;
+
+//udefined
+let noDefinido = undefined;
+
+//symbol
+let simboloUnico = Symbol("Unico");
+
+//BigInt
+let numeroGrande = 2n;
+
+//COMPLEJOS
+
+//object
+let carro = {
+  modelo: "Peugot",
+  kilometraje: 123123,
+  lavado: false,
+};
+
+//array
+let frutas = ["Manzana", "Naranja", "Banana"];
+
+//function
+function saludar(nombre) {
+  console.log("Hola " + nombre);
+}
+
+saludar("Gino");
