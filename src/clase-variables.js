b2053bd683f289664cf25fe20e2352d187b35009
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-12-17 03:46:07 +0000

    Clase 4 del Curso de Fundamentos de JavaScript

---
+++ src/clase-variables.js
@@ -0,0 +1,13 @@
+let cajaDeAndy = "Woody";
+
+console.log(cajaDeAndy);
+
+// // Lo no permitidido:
+// let c = "Woody";
+// let cda = "Woody";
+// let pcAndy = "Woody";
+
+// // Lo permitido
+// let primerTrasteoDeAndy = "Woody";
+// let urlDelUsuario = "https://www.youtube.com/";
+// let idDelUsuario = "123456";
