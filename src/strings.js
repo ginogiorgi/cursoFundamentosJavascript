e8077706b34788a5afe726e50bc15f2c831e08d9
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-12-17 22:35:33 +0000

    Clase 6 del Curso de Fundamentos de JavaScript

---
+++ src/strings.js
@@ -0,0 +1,14 @@
+//STRINGS
+
+let string1 = "Hola mundo ";
+let string2 = "Javcascript es genial";
+let string3 = `${string1} Javcascript es genial`;
+let string4 = string1 + string2;
+let frase = "Javascript es extremademente genial!";
+
+console.log(string3);
+console.log(string4);
+console.log(frase.length);
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+console.log(frase.substring(0, 10).toUpperCase());
